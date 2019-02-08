4a9e0153f1b6a6950747dd1a212004e4cbf4cd03
Mahmudul <mhasan0047@gmail.com>
2019-02-08 01:45:29 -0500

    added amazon defult intents so i can publish it

--- home_automation.py
+++ home_automation.py
@@ -1,4 +1,4 @@
-# Alexa, tell my lamp to turn {OnOff} -> LampOnOffIntent
+# Alexa, tell my lamp to turn {OnOff} -> LampOnOffIntent 
 # Alexa, tell my servo to turn {LeftRight} -> ServoDirectionIntent
 # Alexa, tell my servo to turn {Angle} degrees -> ServoAngleIntent
 
@@ -23,9 +23,14 @@ def launch():
 def fallback():
     return question("I didnt get that. You can say lamp on or off. Or you can say Servo, and then say an angle value")
 
+@ask.intent('AMAZON.CancelIntent')
 @ask.intent('AMAZON.StopIntent')
 def stop():
-    return question("Ok Bye")
+    return statement("Ok Bye")
+
+@ask.intent('Amazon.HelpIntent')
+def help():
+    return question("You can say tell lamp to turn on, or, turn servo 90 degrees")
 
 #LAMP ON/OFF:
 @ask.intent('LampOnOffIntent')
@@ -56,6 +61,10 @@ def servoAngle(Angle):
         formatted_Angle = '%03d' % int(Angle) #turned Angle number to a 3 digit string. SOLUTION to issue where the ESP8266 gets some random numbers passed
         os.system("""mosquitto_pub -t %s -m %s""" %(topic, formatted_Angle) )
         return statement("Turning Servo to Angle: %s" %formatted_Angle)
+
+@ask.intent('ServoDirectionIntent')
+def servoDirection(LeftRight):
+    servoAngle(LeftRight)
 
 if __name__ == '__main__':
     app.run(debug=True)
